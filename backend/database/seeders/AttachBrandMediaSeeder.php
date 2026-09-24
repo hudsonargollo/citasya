707afea38a6f6841b0fa31fe4c52f54203d777cd
Hudson Argollo <hudsonargollo@gmail.com>
2026-09-24 20:59:08 +0200

    feat(media): auto-assign niche service category banners to salons and seed 3D categories

--- backend/database/seeders/AttachBrandMediaSeeder.php
+++ backend/database/seeders/AttachBrandMediaSeeder.php
@@ -27,17 +27,9 @@ class AttachBrandMediaSeeder extends Seeder
         $slides = Slide::all();
         foreach ($slides as $idx => $slide) {
             $slideNum = $idx + 1;
-            $customSlideWebp = public_path("images/slides/slide{$slideNum}.webp");
             $customSlidePath = public_path("images/slides/slide{$slideNum}.png");
 
-            if (file_exists($customSlideWebp)) {
-                $slide->text = '';
-                $slide->button = '';
-                $slide->save();
-                $slide->clearMediaCollection('image');
-                $slide->addMedia($customSlideWebp)->preservingOriginal()->toMediaCollection('image');
-                continue;
-            } elseif (file_exists($customSlidePath)) {
+            if (file_exists($customSlidePath)) {
                 $slide->text = '';
                 $slide->button = '';
                 $slide->save();
@@ -57,30 +49,23 @@ class AttachBrandMediaSeeder extends Seeder
         // 2. Seed Categories
         $categories = Category::all();
         $categoryFiles = [
-            1 => '1_barberia',
-            2 => '2_unas',
-            3 => '3_facial',
-            4 => '4_cejas',
-            5 => '5_spa',
-            6 => '6_maquillaje',
-            7 => '7_estetica',
-            8 => '8_salud',
-            9 => '9_tatuajes',
-            10 => '10_bienestar',
-            11 => '11_fitness',
-            12 => '12_mascotas',
+            1 => '1_barberia.png',
+            2 => '2_unas.png',
+            3 => '3_facial.png',
+            4 => '4_cejas.png',
+            5 => '5_spa.png',
+            6 => '6_maquillaje.png',
+            7 => '7_estetica.png',
+            8 => '8_salud.png',
+            9 => '9_tatuajes.png',
+            10 => '10_bienestar.png',
+            11 => '11_fitness.png',
+            12 => '12_mascotas.png',
         ];
         foreach ($categories as $idx => $category) {
             if (isset($categoryFiles[$category->id])) {
-                $baseName = $categoryFiles[$category->id];
-                $webpPath = public_path("images/categories/{$baseName}.webp");
-                $pngPath = public_path("images/categories/{$baseName}.png");
-
-                if (file_exists($webpPath)) {
-                    $category->clearMediaCollection('image');
-                    $category->addMedia($webpPath)->preservingOriginal()->toMediaCollection('image');
-                    continue;
-                } elseif (file_exists($pngPath)) {
+                $pngPath = public_path("images/categories/" . $categoryFiles[$category->id]);
+                if (file_exists($pngPath)) {
                     $category->clearMediaCollection('image');
                     $category->addMedia($pngPath)->preservingOriginal()->toMediaCollection('image');
                     continue;
@@ -97,7 +82,43 @@ class AttachBrandMediaSeeder extends Seeder
 
         // 3. Seed Salons
         $salons = Salon::all();
+        $serviceCategoryBanners = [
+            1 => 'barber.png',
+            2 => 'nails.png',
+            3 => 'steticists.png',
+            4 => 'steticists.png',
+            5 => 'spa.png',
+            6 => 'steticists.png',
+            7 => 'steticists.png',
+            8 => 'dentists.png',
+            9 => 'services.png',
+            10 => 'spa.png',
+            11 => 'services.png',
+            12 => 'services.png',
+        ];
+
         foreach ($salons as $idx => $salon) {
+            $catId = null;
+            foreach ($salon->eServices as $svc) {
+                $c = $svc->categories()->first();
+                if ($c) {
+                    $catId = $c->id;
+                    break;
+                }
+            }
+            if (!$catId) {
+                $catId = ($idx % 12) + 1;
+            }
+
+            $bannerImg = isset($serviceCategoryBanners[$catId]) ? $serviceCategoryBanners[$catId] : null;
+            $bannerPath = $bannerImg ? public_path('images/services/' . $bannerImg) : null;
+
+            if ($bannerPath && file_exists($bannerPath)) {
+                $salon->clearMediaCollection('image');
+                $salon->addMedia($bannerPath)->preservingOriginal()->toMediaCollection('image');
+                continue;
+            }
+
             $img = $brandImages[($idx + 3) % count($brandImages)];
             $path = public_path('images/brand/' . $img);
             if (file_exists($path)) {
